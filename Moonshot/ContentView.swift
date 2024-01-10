e50c14cdaddd89791ae8a514de9edc1e080942c0
Kirill <K-baranov@outlook.com>
2024-01-10 12:22:30 +0100

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Kirill Baranov on 10/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kirill Baranov on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
